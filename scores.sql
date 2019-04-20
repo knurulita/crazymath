d9c879a2802301ea8daf48491abd66f857b6a959
knurulita <49791651+knurulita@users.noreply.github.com>
2019-04-20 20:20:16 +0700

    Delete scores.sql

--- scores.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `scores` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(30) DEFAULT NULL,
-  `score` int(11) DEFAULT NULL,
-  `playtime` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-)
